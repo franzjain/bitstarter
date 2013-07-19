070f5b9ce41f51238e01f8a4ec3ff0fae32d3afd
franz <franzjain@hotmail.com>
2013-07-18 20:30:19 -0400

    add bootstrap support step one

--- web.js
+++ web.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
-var bufString = fs.readFileSync("index.html");
-var helloString = new Buffer(bufString);
- 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send(helloString.toString());
-});
+app.configure(function(){
+                app.use("/", express.static("/home/franz/git/startup/bitstarter"));
+              });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
